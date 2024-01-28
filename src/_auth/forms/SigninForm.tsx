275457a9abb088762eca632a7d021100d21e87f2
phamanhquyet <q24052002@gmail.com>
2024-01-28 15:54:00 +0700

    make sign in validation and sign in form

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,7 +1,124 @@
-import React from "react";
+import Loader from "@/components/shared/Loader";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/components/ui/use-toast";
+import { useUserContext } from "@/context/AuthContext";
+import { useSignInAccount } from "@/lib/react-query/queriesAndMutations";
+import { SigninValidation } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+import { z } from "zod";
 
 const SigninForm = () => {
-  return <div>SigninForm</div>;
+  const { toast } = useToast();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const navigate = useNavigate();
+
+  const { mutateAsync: signInAccount } = useSignInAccount();
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof SigninValidation>>({
+    resolver: zodResolver(SigninValidation),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof SigninValidation>) {
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    });
+
+    if (!session) {
+      return toast({
+        title: "Đăng nhập thất bại, vui lòng thử lại!",
+      });
+    }
+
+    const isLoggedIn = await checkAuthUser();
+
+    if (isLoggedIn) {
+      form.reset();
+      navigate("/");
+    } else {
+      return toast({
+        title: "Đăng nhập thất bại, vui lòng thử lại!",
+      });
+    }
+  }
+  return (
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" />
+        <h2 className="h3-bold md:h4-bold pt-5 sm:pt-12">
+          Đăng nhập vào tài khoản của bạn
+        </h2>
+        <p className="text-light-3 small-medium md:base-regular mt-2 ">
+          Chào mừng trở lại! Vui lòng nhập thông tin của bạn.
+        </p>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="Email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Mật khẩu</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isUserLoading ? (
+              <div className="flex-center gap-2">
+                <Loader />
+                Đang tải...
+              </div>
+            ) : (
+              "Đăng nhập"
+            )}
+          </Button>
+
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Chưa có tài khoản?
+            <Link
+              to="/sign-up"
+              className="text-primary-500 text-small-semibold ml-1">
+              Đăng ký
+            </Link>
+          </p>
+        </form>
+      </div>
+    </Form>
+  );
 };
 
 export default SigninForm;
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,7 +2,15 @@ import * as z from "zod";
 
 export const SignupValidation = z.object({
   name: z.string().min(2, { message: "Tên quá ngắn" }),
-  username: z.string().min(2).max(50),
+  username: z
+    .string()
+    .min(2, { message: "Tên đăng nhập quá ngắn" })
+    .max(50, { message: "Tên đăng nhập tối đa 50 kí tự" }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Mật khẩu cần dài ít nhất 8 ký tự" }),
+});
+
+export const SigninValidation = z.object({
   email: z.string().email(),
   password: z.string().min(8, { message: "Mật khẩu cần dài ít nhất 8 ký tự" }),
 });
